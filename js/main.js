644d2e63e5fd2ba21c524b8ebaf59e73b338633c
Yevhenii Levinskyi <Divavio615@gmail.com>
2023-02-19 09:46:23 +0200

    add hero page

---
+++ js/main.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // конечная дата
+    const deadline = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 01);
+    // id таймера
+    let timerId = null;
+    // склонение числительных
+    function declensionNum(num, words) {
+        return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
+    }
+    // вычисляем разницу дат и устанавливаем оставшееся времени в качестве содержимого элементов
+    function countdownTimer() {
+        const diff = deadline - new Date();
+        if (diff <= 0) {
+            clearInterval(timerId);
+        }
+        const days = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0;
+        const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+        const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+        const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+        $hours.textContent = hours < 10 ? '0' + hours : hours;
+        $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
+        $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
+        $hours.dataset.title = declensionNum(hours, ['hours']);
+        $minutes.dataset.title = declensionNum(minutes, ['minutes']);
+        $seconds.dataset.title = declensionNum(seconds, ['seconds']);
+    }
+    // получаем элементы, содержащие компоненты даты
+    const $hours = document.querySelector('.magic-box__clock-hours');
+    const $minutes = document.querySelector('.magic-box__clock-minutes');
+    const $seconds = document.querySelector('.magic-box__clock-seconds');
+    // вызываем функцию countdownTimer
+    countdownTimer();
+    // вызываем функцию countdownTimer каждую секунду
+    timerId = setInterval(countdownTimer, 1000);
+});
